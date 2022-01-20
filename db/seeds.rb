477ac8dccf0fd060661d08a1d3be2c82e09b333d
Dennis Neumann <mail@dennis-neumann.com>
2022-01-20 20:52:09 +0100

    Finish index page

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+8.times do
+  Flat.create(
+    name: Faker::Restaurant.name,
+    address: Faker::Address.street_address,
+    description: Faker::ChuckNorris.fact,
+    price_per_night: rand(50..100),
+    number_of_guests: rand(2..6)
+  )
+end
